6d020a361af630bc61a4ca97919417996ad2935f
Deepak <deepuvempati123@gmail.com>
2021-09-16 11:20:25 +0000

    commit changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { TransferComponent } from './transfer/transfer.component';
 const routes: Routes = [
 
-  
-  { path : 'transfer', component:TransferComponent  },
-
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-
+ 
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
 
-  
-  { path: 'check', loadChildren: () => import('./check/check.module').then(m => m.CheckModule) },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { TransferComponent } from './transfer/transfer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavBarComponent,
-    TransferComponent,
-    
+  
   ],
   imports: [
     BrowserModule,
